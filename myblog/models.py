059a67ea9de771c4eb46884a07e0e6af37fc7ad0
mehran1421 <m.kamrani1421@gmail.com>
2021-01-08 18:42:20 +0330

    can use persian slug in url

--- myblog/models.py
+++ myblog/models.py
@@ -1,33 +1,24 @@
+import os
+import random
 from django.db import models
 from django.utils import timezone
+from .utils import unique_slug_generator
+from django.db.models.signals import pre_save
 from extension.utils import jalaly_converter
+from django.template.defaultfilters import slugify
 # Create your models here.
-class Article(models.Model):
-    Status_Choise=(
-        ('d','پیشنویس'),
-        ('p','منتشرشده')
-    )
-    title=models.CharField(max_length=200,verbose_name="تایتل")
-    slug=models.SlugField(max_length=100,verbose_name="عنوان",unique=True)
-    # category=models.ManyToManyField(Category,verbose_name="دسته بندی",related_name="articles")
-    # author=models.ForeignKey(User,null=True,on_delete=models.SET_NULL,related_name="articles",verbose_name="نویسنده")
-    description=models.TextField(verbose_name="توضیحات")
-    thumbnail=models.ImageField(upload_to="images",verbose_name="عکس")
-    publish=models.DateTimeField(default=timezone.now,verbose_name="زمان")
-    created=models.DateTimeField(auto_now_add=True,verbose_name="ساخته ")
-    updated=models.DateTimeField(auto_now=True,verbose_name="آپدیت")
-    status=models.CharField(max_length=1,choices=Status_Choise,verbose_name="وضعیت")
-    class Meta:
-        verbose_name="مقاله"
-        verbose_name_plural="مقاله ها"
 
-    def __str__(self):
-        return self.title
+def get_filename_ext(filepath):
+    base_name = os.path.basename(filepath)
+    name, ext = os.path.splitext(base_name)
+    return name, ext
 
-    def jpublish(self):
-        return jalaly_converter(self.publish)
-
-    jpublish.short_description = "زمان انتشار"
+def upload_image_path(instance, filename):
+    new_name = random.randint(1, 27634723542)
+    name, ext = get_filename_ext(filename)
+    # final_name = f"{new_name}{ext}"
+    final_name = f"{instance.id}-{instance.title}{ext}"
+    return f"products/{final_name}"
 
 class Owner(models.Model):
     title = models.CharField(max_length=200, verbose_name="تایتل")
@@ -48,4 +39,45 @@ class Owner(models.Model):
 
     def __str__(self):
         return self.title
+
+class Article(models.Model):
+    Status_Choise=(
+        ('d','پیشنویس'),
+        ('p','منتشرشده')
+    )
+    title=models.CharField(max_length=200,verbose_name="تایتل")
+    slug=models.SlugField(blank=True,allow_unicode=True,verbose_name="عنوان")
+    # category=models.ManyToManyField(Category,verbose_name="دسته بندی",related_name="articles")
+    # author=models.ForeignKey(User,null=True,on_delete=models.SET_NULL,related_name="articles",verbose_name="نویسنده")
+    description=models.TextField(verbose_name="توضیحات")
+    thumbnail=models.ImageField(upload_to=upload_image_path,verbose_name="عکس")
+    publish=models.DateTimeField(default=timezone.now,verbose_name="زمان")
+    created=models.DateTimeField(auto_now_add=True,verbose_name="ساخته ")
+    updated=models.DateTimeField(auto_now=True,verbose_name="آپدیت")
+    status=models.CharField(max_length=1,choices=Status_Choise,verbose_name="وضعیت")
+    class Meta:
+        verbose_name="مقاله"
+        verbose_name_plural="مقاله ها"
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Article, self).save(*args, **kwargs)
+
+    def jpublish(self):
+        return jalaly_converter(self.publish)
+
+    jpublish.short_description = "زمان انتشار"
+
+
+def product_pre_save_receiver(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = unique_slug_generator(instance)
+
+
+pre_save.connect(product_pre_save_receiver, sender=Article)
+
+
 

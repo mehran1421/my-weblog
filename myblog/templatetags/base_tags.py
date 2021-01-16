96b0915fa47f1a20d77ac078e129182a001a96ba
mehran1421 <m.kamrani1421@gmail.com>
2021-01-17 00:45:41 +0330

    action side bar menue

--- myblog/templatetags/base_tags.py
+++ myblog/templatetags/base_tags.py
@@ -15,3 +15,12 @@ def category_navbar():
         "category":Category.objects.active()
     }
     return context
+
+@register.inclusion_tag("registration/partials/link.html")
+def link(request, link_name, content):
+	return {
+		"request": request,
+		"link_name": link_name,
+		"link": "account:{}".format(link_name),
+		"content": content,
+	}

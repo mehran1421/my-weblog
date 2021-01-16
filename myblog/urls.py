485238c24c71a90c6310abcd9f5abddf9bf3e2d0
mehran1421 <m.kamrani1421@gmail.com>
2021-01-17 01:12:45 +0330

    show preview draft article in site

--- myblog/urls.py
+++ myblog/urls.py
@@ -1,11 +1,12 @@
 from django.urls import re_path,path
-from .views import ArticleList,ArticleDetail,CategoryList,AuthorList
+from .views import ArticleList,ArticleDetail,CategoryList,AuthorList,ArticlePreview
 
 app_name='blog'
 urlpatterns = [
     path('blog/',ArticleList.as_view(),name='articleList'),
     path(r'blog/<int:page>',ArticleList.as_view(),name='articleList'),
     re_path(r'blog/(?P<slug>[-\w\d]+)/', ArticleDetail.as_view(), name="detail"),
+    path('preview/<int:pk>', ArticlePreview.as_view(), name="preview"),
     path('category/<slug:slug>',CategoryList.as_view(),name="category"),
     path('category/<slug:slug>/page/<int:page>', CategoryList.as_view(), name="category"),
     path('author/<slug:username>', AuthorList.as_view(), name="author"),

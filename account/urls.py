f3dddfcb827e0bb1fa68dd84455308093f421d24
mehran1421 <m.kamrani1421@gmail.com>
2021-01-18 21:48:14 +0330

    profile user

--- account/urls.py
+++ account/urls.py
@@ -1,24 +1,33 @@
 from django.urls import path
-from .views import Login,LogOut,ArticleList,ArticleCreate,ArticleUpdate,ArticleDelete,WhyBackArticle
+from .views import (
+    Login,
+    LogOut,
+    ArticleList,
+    ArticleCreate,
+    ArticleUpdate,
+    ArticleDelete,
+    WhyBackArticle,
+    Profile,
+)
 
+# path('logout/', views.LogoutView.as_view(), name='logout'),
+#
+# path('password_change/', views.PasswordChangeView.as_view(), name='password_change'),
+# path('password_change/done/', views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+#
+# path('password_reset/', views.PasswordResetView.as_view(), name='password_reset'),
+# path('password_reset/done/', views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+# path('reset/<uidb64>/<token>/', views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+# path('reset/done/', views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 
-    # path('logout/', views.LogoutView.as_view(), name='logout'),
-    #
-    # path('password_change/', views.PasswordChangeView.as_view(), name='password_change'),
-    # path('password_change/done/', views.PasswordChangeDoneView.as_view(), name='password_change_done'),
-    #
-    # path('password_reset/', views.PasswordResetView.as_view(), name='password_reset'),
-    # path('password_reset/done/', views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    # path('reset/<uidb64>/<token>/', views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    # path('reset/done/', views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
-
-app_name='account'
-urlpatterns =[
-    path('',ArticleList.as_view(),name='home'),
-    path('login/',Login.as_view(),name='login'),
-    path('logout/',LogOut.as_view(),name='logout'),
+app_name = 'account'
+urlpatterns = [
+    path('', ArticleList.as_view(), name='home'),
+    path('login/', Login.as_view(), name='login'),
+    path('logout/', LogOut.as_view(), name='logout'),
     path('article/create', ArticleCreate.as_view(), name="article-create"),
     path('article/update/<int:pk>', ArticleUpdate.as_view(), name="article-update"),
     path('article/back/<int:pk>', WhyBackArticle.as_view(), name="article-back"),
     path('article/delete/<int:pk>', ArticleDelete.as_view(), name="article-delete"),
+    path('profile/', Profile.as_view(), name="profile"),
 ]
--- account/views.py
+++ account/views.py
@@ -2,10 +2,11 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, CreateView,UpdateView,DeleteView,TemplateView
 from django.urls import reverse_lazy
 from django.shortcuts import render
+from .forms import ProfileForm
 from django.contrib.auth.views import LoginView,LogoutView
 from .mixins import FieldsMixin,FormValidMixin,BackAccessMixin,AuthorAccessMixin,AuthorsAccessMixin,SuperUserAccessMixin
 from myblog.models import Article
-
+from .models import User
 
 # Create your views here.
 
@@ -28,10 +29,12 @@ class ArticleUpdate(AuthorAccessMixin,FormValidMixin, FieldsMixin, UpdateView):
 	model = Article
 	template_name = "registration/article-create-update.html"
 
+
 class ArticleDelete(SuperUserAccessMixin,DeleteView):
     model = Article
     template_name = 'registration/article_confirm_delete.html'
     success_url = reverse_lazy('account:home')
+
 
 class WhyBackArticle(BackAccessMixin,TemplateView):
     def get(self,request,*args,**kwargs):
@@ -41,6 +44,22 @@ class WhyBackArticle(BackAccessMixin,TemplateView):
         }
         return render(request,'registration/whyback.html',context)
 
+
+class Profile(UpdateView):
+    model = User
+    form_class = ProfileForm
+    template_name = "registration/profile.html"
+    success_url = reverse_lazy("account:profile")
+
+    def get_object(self, queryset=None):
+        return User.objects.get(pk=self.request.user.pk)
+
+    def get_form_kwargs(self):
+        kwargs=super(Profile,self).get_form_kwargs()
+        kwargs.update({
+            'user':self.request.user
+        })
+        return kwargs
 
 
 class Login(LoginView):

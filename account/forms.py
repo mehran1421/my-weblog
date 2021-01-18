ffd75747d148cc8bc33065f48b701a0b5ca2a6c1
mehran1421 <m.kamrani1421@gmail.com>
2021-01-18 21:48:14 +0330

    profile user

--- account/forms.py
+++ account/forms.py
@@ -46,3 +46,18 @@ class RegisterForm(UserCreationForm):
             return email
         raise forms.ValidationError("این ایمیل قبلا توسط شخص دیگری استفاده شده است")
 
+class ProfileForm(forms.ModelForm):
+    def __init__(self,*args,**kwargs):
+        user=kwargs.pop('user')
+        super(ProfileForm,self).__init__(*args,**kwargs)
+
+        if not user.is_superuser:
+            self.fields['username'].disabled=True
+            self.fields['email'].disabled=True
+            self.fields['special_user'].disabled=True
+        self.fields['username'].help_text='نام کاربری خود در هنگام ثبت نام وارد شده و قابل تغییر نیست'
+
+    class Meta:
+        model=User
+        fields = ['username', 'email', 'first_name', 'last_name', 'special_user', 'is_author']
+
